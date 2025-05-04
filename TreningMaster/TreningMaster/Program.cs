f9f0096289e31ecfb262c10c8c57e7ea576a6fe9
Izabella Sochacka <sochacka@cyberstudio.pl>
2025-05-04 19:26:28 +0200

    Zadanie 7

--- TreningMaster/TreningMaster/Program.cs
+++ TreningMaster/TreningMaster/Program.cs
@@ -16,65 +16,92 @@ namespace TreningMaster
         public enum MuscleParts
         {
             back = 1,
-            buttocks = 2,
-            thighs = 3,
-            calves = 4,
+            leg = 2,
+            chest = 3,
+            biceps = 4,
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Dostępne partie mięśniowe:");
-            foreach (MuscleParts part in Enum.GetValues(typeof(MuscleParts)))
+            //Console.WriteLine("Dostępne partie mięśniowe:");
+            //foreach (MuscleParts part in Enum.GetValues(typeof(MuscleParts)))
+            //{
+            //    Console.WriteLine($"{(int)part} - {part}");
+            //}
+
+            List<Exercise> exercises = new List<Exercise>();
+            //{
+            //    new Exercise()
+            //    {
+            //        Name = "Monster Walk",
+            //        MusclePart = MuscleParts.back,
+            //    },
+            //     new Exercise()
+            //    {
+            //        Name = "Glute Bridge",
+            //        MusclePart = MuscleParts.back,
+            //    },
+            //};
+
+            string line;
+            try
             {
-                Console.WriteLine($"{(int)part} - {part}");
-            }
-
-            //["Monster Walk", "Glute Bridge", "Arch hold"];
-
-            // Monster Walk;3
-
-            List<Exercise> exercises = new List<Exercise>()
-            {
-                new Exercise()
+                StreamReader sr = new StreamReader(@"C:\Users\Izabella\Documents\Visual Studio 2022\Zadanie.txt");
+                line = sr.ReadLine();
+                while (line != null)
                 {
-                    Name = "Monster Walk",
-                    MusclePart = MuscleParts.back,
-                },
-                 new Exercise()
-                {
-                    Name = "Glute Bridge",
-                    MusclePart = MuscleParts.back,
-                },
-            };
+                    Console.WriteLine(line);
 
-            ///////////////////////////////
-            Console.WriteLine("Dodaj nowe ćwiczenia:");
+                    string[] parts = line.Split(';');
 
-            while (true)
-            {
-                // {nazwa};{number_partii}
-                string input = Console.ReadLine();
-                if (input == "exit")
-                {
-                    break;
+                    string name = parts[0];
+                    MuscleParts musclePart = Enum.Parse<MuscleParts>(parts[1]);
+
+                    exercises.Add(new Exercise { Name = name, MusclePart = musclePart });
+
+                    line = sr.ReadLine();
                 }
-                string[] parts = input.Split(';');
-
-                string name = parts[0];
-                MuscleParts musclePart = Enum.Parse<MuscleParts>(parts[1]);
-
-                exercises.Add(new Exercise { Name = name, MusclePart = musclePart });
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Koniec czytania pliku\n");
             }
 
-            ///////////////////////////////
+           
+            //Console.WriteLine("Dodaj nowe ćwiczenia:");
 
-            Console.WriteLine("Lista ćwiczeń na pośladki:");
+            //while (true)
+            //{
+            //    // {nazwa};{number_partii}
+            //    string input = Console.ReadLine();
+            //    if (input == "exit")
+            //    {
+            //        break;
+            //    }
+            //    string[] parts = input.Split(';');
+
+            //    string name = parts[0];
+            //    MuscleParts musclePart = Enum.Parse<MuscleParts>(parts[1]);
+
+            //    exercises.Add(new Exercise { Name = name, MusclePart = musclePart });
+            //}
+
+
+            
+
+            Console.WriteLine("Lista ćwiczeń:");
             foreach (var exercise in exercises)
             {
                 Console.WriteLine("\t" + exercise.Name + "\t" + exercise.MusclePart);
             }
+            Console.WriteLine();
 
-            Console.WriteLine("Ćwiczenia");
+            Console.WriteLine("Ćwiczenia:");
             foreach (MuscleParts part in Enum.GetValues(typeof(MuscleParts)))
             {
                 Console.WriteLine(part);
@@ -86,34 +113,34 @@ namespace TreningMaster
                     }
                 }
             }
-            Console.WriteLine("Sortowanie od a do z:");
-            exercises.Sort((x, y) => x.Name.CompareTo(y.Name));//pod foreach nie dziłało, czemu tak?
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine("\t" + exercise.Name);
-            }
+            //Console.WriteLine("Sortowanie od a do z:");
+            //exercises.Sort((x, y) => x.Name.CompareTo(y.Name));//pod foreach nie dziłało, czemu tak?
+            //foreach (var exercise in exercises)
+            //{
+            //    Console.WriteLine("\t" + exercise.Name);
+            //}
 
-            Console.WriteLine("A teraz od z do a:");
-            exercises.Sort((x, y) => y.Name.CompareTo(x.Name));
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine("\t" + exercise);
-            }
+            //Console.WriteLine("A teraz od z do a:");
+            //exercises.Sort((x, y) => y.Name.CompareTo(x.Name));
+            //foreach (var exercise in exercises)
+            //{
+            //    Console.WriteLine("\t" + exercise);
+            //}
 
-            Console.WriteLine("A teraz od najkrótszego ciągu:");
-            exercises.Sort((x, y) => x.Name.Length.CompareTo(y.Name.Length)); //tu potrzebuje pomocy 
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine("\t" + exercise);
-            }
+            //Console.WriteLine("A teraz od najkrótszego ciągu:");
+            //exercises.Sort((x, y) => x.Name.Length.CompareTo(y.Name.Length)); //tu potrzebuje pomocy 
+            //foreach (var exercise in exercises)
+            //{
+            //    Console.WriteLine("\t" + exercise);
+            //}
 
 
-            Console.WriteLine("A teraz od A do Z tylko po ostatniej literze w nazwie:");
-            exercises.Sort((x, y) => x.Name[x.Name.Length - 1].CompareTo(y.Name[y.Name.Length - 1])); //tu potrzebuje pomocy 
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine("\t" + exercise);
-            }
+            //Console.WriteLine("A teraz od A do Z tylko po ostatniej literze w nazwie:");
+            //exercises.Sort((x, y) => x.Name[x.Name.Length - 1].CompareTo(y.Name[y.Name.Length - 1])); //tu potrzebuje pomocy 
+            //foreach (var exercise in exercises)
+            //{
+            //    Console.WriteLine("\t" + exercise);
+            //}
 
         }
 
