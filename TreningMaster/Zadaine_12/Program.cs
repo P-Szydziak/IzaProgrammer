09de9aa1376c6e526c443cd7f2f60c5a82a3b6f0
Izabella Sochacka <sochacka@cyberstudio.pl>
2025-05-15 21:33:32 +0200

    Zadanie 12

---
+++ TreningMaster/Zadaine_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Zadaine_12;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Application excel = new Application();
+        // Create a new workbook
+        Workbook workbook = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+        // Get the first worksheet in the workbook
+        Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+        worksheet.Name = "multiplication table";
+        // Set the value of a cell
+        for(int row = 1; row <= 10; row++)
+        {
+            for (int column = 1; column <= 10; column++)
+            {
+                worksheet.Cells[row, column] = row * column;
+            }
+        }
+
+        // Save the workbook to a file
+        workbook.SaveAs("Zadanie_12");
+        // Close the workbook and Excel application
+        workbook.Close();
+
+        excel.Quit();
+    }
+}
