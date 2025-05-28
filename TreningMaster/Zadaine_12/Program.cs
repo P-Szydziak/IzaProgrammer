37a32c6c404f3580b22ed54c6fb76eb534b16865
Izabella Sochacka <sochacka@cyberstudio.pl>
2025-05-28 11:59:30 +0200

    Zadanie_19

--- TreningMaster/Zadaine_12/Program.cs
+++ TreningMaster/Zadaine_12/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Data.Common;
-using System.Drawing;
-using System.Text;
+﻿using System.Drawing;
 using Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
 
 namespace Zadaine_12;
 
@@ -10,59 +8,22 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Application excel = new Application();
+        int size = GetTableSizeFromArgs(args, defaultSize: 10);
+        if (args.Length > 0)
+        {
+            Console.WriteLine(args[0]);
+        }
+        var excel = new Application();
         // Create a new workbook
-        Workbook workbook = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+        var workbook = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
         // Get the first worksheet in the workbook
-        Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+        var worksheet = (Worksheet)workbook.Worksheets[1];
         worksheet.Name = "multiplication table";
-        // Set the value of a cell
-        for (int row = 1; row <= 10; row++)
-        {
-            for (int column = 1; column <= 10; column++)
-            {
-                worksheet.Cells[row, column] = row * column;
-                if (row * column % 2 == 0)
-                {
-                    worksheet.Cells[row, column].Font.Color = System.Drawing.Color.Green;
-                }
 
-            }
-        }
-
-        Microsoft.Office.Interop.Excel.Range chartRange2;
-        chartRange2 = worksheet.get_Range("j1", "j10");
-        for (int i = 1; i <= 10; i++)
-        {
-            worksheet.Cells[1, i].Interior.Color = System.Drawing.Color.Pink;
-            worksheet.Cells[i, 1].Interior.Color = System.Drawing.Color.Pink;
-            worksheet.Cells[10, i].Interior.Color = System.Drawing.Color.Pink;
-            worksheet.Cells[i, 10].Interior.Color = System.Drawing.Color.Pink;
-
-            string Letter = ((char)('A' + i - 1)).ToString(); //Ustawienie "A" jako pierwszej kolumny i zmiana tego na string - char
-            worksheet.Cells[11, i].Formula = $"=SUM({Letter}1:{Letter}10)";
-            worksheet.Cells[11, i].Interior.Color = System.Drawing.Color.SandyBrown;
-            worksheet.Cells[11, i].Font.Color = System.Drawing.Color.MediumPurple;
-            worksheet.Cells[11, i].Font.Italic = true;
-            worksheet.Cells[11, i].Font.Bold = true;
-        }
-
-        Microsoft.Office.Interop.Excel.Range chartRange3;
-        chartRange3 = worksheet.get_Range("a10", "j10");
-        chartRange3.Font.Color = System.Drawing.Color.Red;
-
-        Microsoft.Office.Interop.Excel.Range chartRange4;
-        chartRange4 = worksheet.get_Range("a1", "j10");
-        foreach (Microsoft.Office.Interop.Excel.Range cell in chartRange4.Cells)
-        {
-            cell.BorderAround2();
-        }
-
-        Microsoft.Office.Interop.Excel.Range chartRange;
-        chartRange = worksheet.get_Range("a1", "j10");
-        chartRange.BorderAround2(Weight: XlBorderWeight.xlThick);
-
-        
+        FillMultiplicationTable(worksheet, size);
+        HighlightBordersAndHeaders(worksheet, size);
+        AddSumFormulas(worksheet, size);
+        FormatBorders(worksheet, size);
 
         // Save the workbook to a file
         workbook.SaveAs("Zadanie_12");
@@ -71,4 +32,85 @@ public class Program
 
         excel.Quit();
     }
+
+    static int GetTableSizeFromArgs(string[] args, int defaultSize)
+    {
+        if (args.Length > 0 && int.TryParse(args[0], out int parsedSize) && parsedSize > 0)
+        {
+            return parsedSize;
+        }
+
+        return defaultSize;
+    }
+
+    static void FillMultiplicationTable(Worksheet worksheet, int size)
+    { // Set the value of a cell
+        for (int row = 1; row <= size; row++)
+        {
+            for (int column = 1; column <= size; column++)
+            {
+                worksheet.Cells[row, column] = row * column;/// tu może być błąd
+                if (row * column % 2 == 0)
+                {
+                    worksheet.Cells[row, column].Font.Color = Color.Green;
+                }
+
+            }
+        }
+    }
+
+    
+    static void HighlightBordersAndHeaders(Worksheet worksheet, int size)
+    { 
+       
+        for (int i = 1; i <= size; i++)
+        {
+            worksheet.Cells[1, i].Interior.Color = Color.Pink;
+            worksheet.Cells[i, 1].Interior.Color = Color.Pink;
+            worksheet.Cells[size, i].Interior.Color = Color.Pink;
+            worksheet.Cells[i, size].Interior.Color = Color.Pink;
+        }
+        var bottomRowRange = worksheet.get_Range($"A{size}", $"{ToExcelColumn(size)}{size}");/// tu może być błąd
+        bottomRowRange.Font.Color = ColorTranslator.ToOle(Color.Red);
+    }
+
+    static void AddSumFormulas(Worksheet worksheet, int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            string Letter = ((char)('A' + i - 1)).ToString(); //Ustawienie "A" jako pierwszej kolumny i zmiana tego na string - char
+            worksheet.Cells[11, i].Formula = $"=SUM({Letter}1:{Letter}10)";
+            worksheet.Cells[11, i].Interior.Color = Color.SandyBrown;
+            worksheet.Cells[11, i].Font.Color = Color.MediumPurple;
+            worksheet.Cells[11, i].Font.Italic = true;
+            worksheet.Cells[11, i].Font.Bold = true;
+        }
+      
+    }
+
+
+    static void FormatBorders(Worksheet worksheet, int size)
+    {
+        var fullRange = worksheet.get_Range("A1", $"{ToExcelColumn(size)}{size}");
+        foreach (Range cell in fullRange.Cells)
+        {
+            cell.BorderAround2();
+        }
+
+        fullRange.BorderAround2(Weight: XlBorderWeight.xlThick);
+    }
+
+
+    static string ToExcelColumn(int columnNumber)
+    {
+        string columnName = "";
+        while (columnNumber > 0)
+        {
+            int remainder = (columnNumber - 1) % 26;
+            columnName = (char)(remainder + 'A') + columnName;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        return columnName;
+    }
+
 }
